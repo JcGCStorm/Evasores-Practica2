d0288864c872ef33af330d25874b8e1c078d0b6c
Rafael <rafaelcastro@ciencias.unam.mx>
2024-03-08 15:58:19 -0600

    Cambio de lugar de las clases de Template. Se agregaron las ultimas hamburguesas

---
+++ src/HamburguesaCrazy8.java
@@ -0,0 +1,66 @@
+/**
+ * La clase Hamburguesa Crazy8 representa una hamburguesa tipo Crazy8
+ * Extiende la clase abstracta Hamburguesa e implementa métodos específicos para su preparación 
+ */
+public class HamburguesaCrazy8 extends Hamburguesa {
+
+    /**
+    * Constructor para crear una nueva HamburguesaCrazy8
+    * 
+    */
+   public HamburguesaCrazy8() {
+       super.nombre = "Hamburguesa Niño Pobre";
+       super.id = 203;
+       super.descripcion = "Si lo que quieres es una experiencia 99.9% pura, esta hamburguesa es para ti. Con carne directamente traída de los pollos Hermanos y un ingrediente secreto. $400";
+       super.tieneQueso = false;
+       super.esVegetariana = true;
+   }
+
+   /**
+    * Se pone el pan 
+    */
+   @Override
+   protected void ponerPan() {
+       System.out.println("Poniendo pan");
+   }
+
+   /**
+    * Se pone la mayonesa 
+    */
+   @Override
+   protected void ponerMayonesa() {
+       System.out.println("Poniendo mayonesa");
+   }
+
+   /**
+    * Se pone la mostaza
+    */
+   @Override
+   protected void ponerMostaza() {
+       System.out.println("Poniendo mostaza");
+   }
+
+   /**
+    * Se prepara la carne 
+    */
+   @Override
+   protected void prepararCarne() {
+       System.out.println("Preparando nuggets de pollo de los Pollos Hermanos, con polvo azul secreto");
+   }
+
+   /**
+    * Se pone los vegetales
+    */
+   @Override
+   protected void ponerVegetales() {
+       System.out.println("Poniendo vegetales. Lechuga y jitomates.");
+   }
+
+   /**
+    * Se pone la catsup 
+    */
+   @Override
+   protected void ponerCatsup() {
+       System.out.println("Poniendo catsup, con un toque de chipótle");
+   }
+}
